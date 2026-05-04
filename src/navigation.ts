b9dff64bab4adb71351c09ce5378739931a72cbe
Andrea Giardinicd <giardiniandrea7@gmail.com>
2026-05-04 14:20:05 +0200

    Sostituisce sito vecchio con nuova versione Astro
    Migrazione da HTML statico a progetto Astro 5 + Tailwind
       con nuovo design brand giardiniconsulenza

---
+++ src/navigation.ts
@@ -0,0 +1,53 @@
+import { getPermalink } from './utils/permalinks';
+ 
+export const headerData = {
+  links: [
+    { text: 'Chi sono', href: '/#about' },
+    { text: 'Percorso', href: '/#resume' },
+    { text: 'Servizi', href: '/#portfolio' },
+    { text: 'Contatti', href: 'mailto:andrea@giardiniconsulenza.it' },
+  ],
+  actions: [
+    {
+      text: 'Scrivimi',
+      href: 'mailto:andrea@giardiniconsulenza.it',
+    },
+  ],
+};
+ 
+export const footerData = {
+  links: [
+    {
+      title: 'Scopri',
+      links: [
+        { text: 'Chi sono', href: '/chi-sono' },
+        { text: 'Richiedi diagnosi gratuita', href: '/richiedi-diagnosi' },
+      ],
+    },
+    {
+      title: 'Contatti',
+      links: [
+        { text: 'andrea@giardiniconsulenza.it', href: 'mailto:andrea@giardiniconsulenza.it' },
+        { text: '+39 351 545 6845', href: 'tel:+393515456845' },
+        { text: 'LinkedIn', href: 'https://www.linkedin.com/in/andrea-giardini-58923a268' },
+        { text: 'Instagram', href: 'https://www.instagram.com/andreagiardini.finanza/' },
+      ],
+    },
+    {
+      title: 'Informazioni',
+      links: [
+        { text: 'Privacy Policy', href: '/privacy' },
+        { text: 'Cookie Policy', href: '/cookie-policy' },
+        { text: 'Note legali', href: '/note-legali' },
+      ],
+    },
+  ],
+  secondaryLinks: [],
+  socialLinks: [
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/andrea-giardini-58923a268' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/andreagiardini.finanza/' },
+    { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: '#' },
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:andrea@giardiniconsulenza.it' },
+  ],
+  footNote: '',
+};
